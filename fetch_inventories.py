e9a0505f2ca8b97e128bbe2820af665d3a60019f
Fillbert Halim <fhalim3-c@my.cityu.edu.hk>
2025-08-21 17:38:02 +0800

    added dockerfile

--- fetch_inventories.py
+++ fetch_inventories.py
@@ -9,13 +9,8 @@ def fetch_categories(roblox_id: str):
     return pd.DataFrame([{'Name': i.get('items')[0].get('displayName'), 'ID': i.get('items')[0].get('id')} for i in categories])
 
 def fetch_inventory(roblox_id: str, category_id: str):
-    response = requests.get(f"https://inventory.roblox.com/v1/users/{roblox_id}/inventory/{category_id}")
-    response = json.loads(response.text)['data']
-    responses = []
-    for i in response:
-        responses.append(json.loads(requests.get(f"https://catalog.roblox.com/v1/catalog/items/{i}/details?itemType=Asset").text))
-        time.sleep(2)
-    # responses = pd.DataFrame(responses, columns=["Items"])
-    return responses
-
-print(fetch_inventory("171322313", "2"))
+    response = requests.get(f"https://inventory.roblox.com/v2/users/{roblox_id}/inventory/{category_id}")
+    items = []
+    for i in json.loads(response.text)['data']:
+        items.append((i['assetId'], i['assetName']))
+    return pd.DataFrame(items)
